99af987f0d559ca0b14d920ade2acda66e20074f
iDome89 <44414108+iDome89@users.noreply.github.com>
2019-10-21 14:40:55 +0200

    a

--- src/components/model/PricingContext.js
+++ src/components/model/PricingContext.js
@@ -1,14 +1,23 @@
 import React, { useState, createContext } from "react";
 export const PricingContext = createContext();
 export const PricingProvider = props => {
-  const [prices, setPrices] = useState([
+  const [items, setItems] = useState([
     {
-      name: "Apple",
-      price: 2000
+      totalPrice: [],
+      firstChoice: [
+        { name: "Apple", price: 2000 },
+        { name: "Android", price: 1500 },
+        { name: "Both", price: 3500 }
+      ],
+      secondChoice: [
+        { name: "Yes", price: 2400 },
+        { name: "No", price: 0 },
+        { name: "Idk", price: 0 }
+      ]
     }
   ]);
   return (
-    <PricingContext.Provider value={[prices, setPrices]}>
+    <PricingContext.Provider value={[items, setItems]}>
       {props.children}
     </PricingContext.Provider>
   );
