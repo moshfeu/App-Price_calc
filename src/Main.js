41458b94179cf7ff936ac7a8e481a6d596f1ee11
iDome89 <dominik.zecoli@gmail.com>
2019-11-05 16:53:48 +0000

    refactoring

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Frontpage from "./components/Frontpage";
-import Social from "./components/questions/Social";
-import Payments from "./components/questions/Payments";
-import Logo from "./components/questions/Logo";
 import Price from "./components/Price";
 import Recap from "./components/Recap";
-import Type from "./components/questions/Type";
+import Choices from "./components/questions/Choices";
 import { PagesProvider } from "./components/model/PagesContext";
 import { PreviousPage } from "./components/StyledComponents";
-
+import Model from "./components/model/questions.model/Model"
 function Main() {
 
   return (
@@ -30,9 +27,6 @@ function Main() {
             }}
           />
           <Route exact path="/questions/type" component={Choices} />
-          <Route exact path="/questions/social" component={Social} />
-          <Route exact path="/questions/payments" component={Payments} />
-          <Route exact path="/questions/logo" component={Logo} />
         </PagesProvider>
       </div>
     </Router>
