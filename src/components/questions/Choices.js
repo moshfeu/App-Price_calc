c75748ac2fecbb8f13b779d9c74c406cab23194b
iDome89 <44414108+iDome89@users.noreply.github.com>
2019-11-06 15:28:00 +0100

    Testing Handle events

--- src/components/questions/Choices.js
+++ src/components/questions/Choices.js
@@ -1,5 +1,5 @@
-import React from "react";
-/*import PagesContext from "../model/PagesContext";*/
+import React, { useContext } from "react";
+import PagesContext from "../model/PagesContext";
 import {
   MainWrapper,
   MainText,
@@ -10,21 +10,27 @@ import {
   RouteLink
 } from "../StyledComponents";
 
-const Choices = ({pagename, values}) => {
+const Choices = ({ pagename, values }) => {
+  const { price, setPrice } = useContext(PagesContext);
+  const AddPrice = e => {
+    setPrice([e.target.id, ...price]);
+  };
   return (
     <MainWrapper>
       <MainText>{pagename}</MainText>
       <ButtonsWrapper>
-        {
-        values.map(button=>(
-        <SelectionsContainer>
-          <RouteLink to={button.next}>
-            <Icon src={button.icon} key={button.id}/>
-          </RouteLink>
-          <ButtonLabel>{button.name}</ButtonLabel>
-        </SelectionsContainer>
-          ))
-        }
+        {values.map(button => (
+          <SelectionsContainer>
+            <RouteLink
+              onClick={AddPrice()}
+              to={button.next}
+              price={button.price}
+            >
+              <Icon src={button.icon} key={button.id} />
+            </RouteLink>
+            <ButtonLabel>{button.name}</ButtonLabel>
+          </SelectionsContainer>
+        ))}
       </ButtonsWrapper>
     </MainWrapper>
   );
