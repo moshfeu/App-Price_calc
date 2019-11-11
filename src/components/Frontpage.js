9de57f842d9ecf0cfcac43e4e2a6e40d2ee5c58b
iDome89 <44414108+iDome89@users.noreply.github.com>
2019-11-11 12:28:39 +0100

    Reviewed previous page button and fixed, fixed also frontPage

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { CoverImage,
-    FrontWrapper,
-    MainTitle,
-    SubText,
-    StartButton} from './StyledComponents'
+import React, { useContext } from "react";
+import {
+  CoverImage,
+  FrontWrapper,
+  MainTitle,
+  SubText,
+  StartButton
+} from "./StyledComponents";
+import { PagesContext } from "./model/PagesContext";
 
-
- const Frontpage = () => {
-     const handleStart = () => {
-         window.location.href = "/questions/type";;
-     };
-    return (
-        <FrontWrapper> 
-            <CoverImage />
-            <MainTitle>Do you want to know how much an app will cost you?</MainTitle>
-            <SubText> If the answer is YES, click on the start button below!</SubText>
-            <StartButton onClick={handleStart}>Start!</StartButton>
-        </FrontWrapper>
-    )
-}
+const Frontpage = () => {
+  const context = useContext(PagesContext);
+  const handleStart = () => {
+    context[2].push("/questions/type");
+  };
+  return (
+    <FrontWrapper>
+      <CoverImage />
+      <MainTitle>Do you want to know how much an app will cost you?</MainTitle>
+      <SubText> If the answer is YES, click on the start button below!</SubText>
+      <StartButton onClick={handleStart}>Start!</StartButton>
+    </FrontWrapper>
+  );
+};
 export default Frontpage;
-
--- src/components/Recap.js
+++ src/components/Recap.js
@@ -2,9 +2,8 @@ import React, { useContext } from "react";
 import { PagesContext } from "./model/PagesContext";
 import Price from "../components/Price";
 
-
 function Recap() {
-  const [price, setPrice, history]=useContext(PagesContext);
+  const [price, setPrice, history] = useContext(PagesContext);
   const cleanUp = () => {
     setPrice(price.slice(0));
     history.push("/");
@@ -12,7 +11,12 @@ function Recap() {
   return (
     <>
       <Price />
-      <button  style={{ width: "100px", height: "100px", color: "white"}} onClick={cleanUp}>Restart</button>
+      <button
+        style={{ width: "100px", height: "100px", color: "white" }}
+        onClick={cleanUp}
+      >
+        Restart
+      </button>
     </>
   );
 }
