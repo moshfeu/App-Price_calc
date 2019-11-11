22011498eebd785e8bd6f12d1018f4f9bf25a63f
iDome89 <44414108+iDome89@users.noreply.github.com>
2019-11-11 12:28:39 +0100

    Reviewed previous page button and fixed, fixed also frontPage

--- src/components/Price.js
+++ src/components/Price.js
@@ -3,12 +3,10 @@ import { Pricing } from "./StyledComponents";
 import { PagesContext } from "./model/PagesContext";
 
 export default function Price(props) {
-  const price = useContext(PagesContext);
-  console.log(price);
+  const context = useContext(PagesContext);
   // price is basically the array [price, setPrice, history], so you probably want the first item
-  const midPrice = price[0].map((value) => (value.price));
-  const finalPrice = midPrice.reduce((a,b) => a + b, 0);
-  console.log(midPrice);
+  const midPrice = context[0].map(value => value.price);
+  const finalPrice = midPrice.reduce((a, b) => a + b, 0);
   return (
     <div>
       <Pricing>{finalPrice}</Pricing>
